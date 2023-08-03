121aef3f986ce0d85edb04ce55aa5da902db1de4
Curtis O'Dell <curtisodell1@gmail.com>
2023-08-02 19:06:03 -0600

    hooks and props

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Curtis",
+  city: "Boulder",
   bio: "I made this!",
-  color: "firebrick",
+  color: "pink",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/Curtisodell1",
+    linkedin: "https://www.linkedin.com/in/curtisodell1/",
   },
 };
 
